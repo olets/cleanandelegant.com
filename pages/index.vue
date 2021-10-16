ee25e01ab7f8a2a719845c7b2adc3b9d50533562
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-15 18:28:30 -1000

    refactor(data): define in vue file

--- pages/index.vue
+++ pages/index.vue
@@ -31,8 +31,88 @@
 </template>
 
 <script>
-  import { airtableSections } from '../constants/airtableSections'
-  import { linkLists } from '../constants/linkLists'
+  // dividerClassNames must be safelisted in tailwind.config.js
+  const airtableSections = [
+    {
+      heading: 'Select CV',
+      dividerClassName: 'bg-green-700',
+      airtableEmbedId: 'shrSQfzb1yfob4QOe',
+      summary: 'Some recent projects, sorted by date with ongoing projects first and then alphabetically.',
+      airtableEmbedTitle: 'CV table'
+    },
+    {
+      heading: 'Select Writings',
+      dividerClassName: 'bg-green-700',
+      airtableEmbedId: 'shrDTpZKkXftcjYOk',
+      airtableEmbedTitle: 'Writings table'
+    },
+  ]
+
+  // hover color classes must be safelisted in tailwind.config.js
+  const linkLists = [
+    {
+      heading: 'Experience',
+      links: [
+        {
+          className: 'underline',
+          href: '/Henry_Bley-Vroman_resume.pdf',
+          hoverColor: 'maroon-400',
+          iconEntity: '&#xe415;',
+          text: 'Print-ready Resume',
+          textClasses: 'font-bold',
+        },
+        {
+          className: 'underline',
+          href: 'https://stackoverflow.com/story/henrybleyvroman',
+          hoverColor: 'yellow-500',
+          iconEntity: '&#xeA19;',
+          text: 'Stackoverflow Developer Story (CV)',
+        },
+      ],
+    },
+    {
+      heading: 'Contact',
+      links: [
+        {
+          className: 'underline',
+          href: 'mailto:olets@olets.dev',
+          hoverColor: 'green-500',
+          iconEntity: '&#xe0E6;',
+          text: 'olets@olets.dev',
+        },
+        {
+          iconEntity: '&#xe0c8;',
+          text: 'Hawaii',
+        },
+      ],
+    },
+    {
+      heading: 'Profiles',
+      links: [
+        {
+          className: 'underline',
+          href: 'https://github.com/olets',
+          hoverColor: 'pink-500',
+          iconEntity: '&#xe86f;',
+          text: 'GitHub projects & open-source contributions',
+        },
+        {
+          className: 'underline',
+          href: 'https://stackoverflow.com/users/1241736/henry',
+          hoverColor: 'yellow-500',
+          iconEntity: '&#xe0c6;',
+          text: 'Stackoverflow activity',
+        },
+        {
+          className: 'underline',
+          href: 'https://www.linkedin.com/in/henry-bley-vroman',
+          hoverColor: 'blue-600',
+          iconEntity: '&#xe97a;',
+          text: 'LinkedIn',
+        },
+      ],
+    },
+  ]
 
   export default {
     data() {
