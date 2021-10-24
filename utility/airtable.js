3753bbff600ef41e1a1532154458e4c345739deb
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-24 08:45:11 -1000

    feat(github actions): support airtable

--- utility/airtable.js
+++ utility/airtable.js
@@ -2,7 +2,7 @@ const Airtable = require('airtable')
 
 export const airtableFields = (table, config = {}) => {
   const fields = []
-  const base = new Airtable({apiKey: process.env.airtableApiKey}).base(process.env.airtableBase);
+  const base = new Airtable({apiKey: 'keyo6KR00X6wS4DUS'}).base('appZ2chc7ofUIoLuN');
   base(table).select(config)
     .eachPage(function page(records, fetchNextPage) {
       records.forEach((r) => fields.push(r.fields))
