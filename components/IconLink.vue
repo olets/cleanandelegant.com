28ad1070d2b9838cea951ea8b01658f391c9c529
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-24 09:18:25 -1000

    feat(IconLink): no <a> if no href

--- components/IconLink.vue
+++ components/IconLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :class="`mb-4 flex ${className}`"
+  <a v-if="href" :class="`mb-4 flex ${className}`"
         :href="href">
     <span class="mt-px mr-2 transition-all font-icon"
           aria-hidden="true">
@@ -9,6 +9,15 @@
 
     <span :class="`transition-all ${textClass}`">{{ text }}</span>
   </a>
+  <div v-else :class="`mb-4 flex ${className}`">
+    <span class="mt-px mr-2 transition-all font-icon"
+          aria-hidden="true">
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <span v-html="iconEntity"></span>
+    </span>
+
+    <span :class="`transition-all ${textClass}`">{{ text }}</span>
+  </div>
 </template>
 
 <script>
