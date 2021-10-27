284dbb7827096852b660f75484fa86ec72ea09e1
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-26 20:19:41 -1000

    feat(footer): list tech

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,8 @@
     <div class="leading-loose">
       <p>
         Site copyright &copy; {{ year }} Henry Bley-Vroman.
-        See the source at <a class="underline transition-all focus:text-pink-500 hover:text-pink-500 underline-pink-500" href="https://github.com/olets/cleanandelegant.com">https://github.com/olets/cleanandelegant.com</a>
+        <br>
+        See the source (Nuxt 2 (Vue 2), Tailwind CSS 3, Airtable data over REST API, GitHub Actions workflows) at <a class="underline transition-all focus:text-pink-500 hover:text-pink-500 underline-pink-500" href="https://github.com/olets/cleanandelegant.com">https://github.com/olets/cleanandelegant.com</a>
         <br>
         Photo copyright &copy; 2016 Bradley Aaron Photography.
         <br>
