b403274793bae703362e785f1a425ac5752daad4
Henry Bley-Vroman <olets@olets.dev>
2023-09-08 14:28:26 -1000

    feat(site): new static vite...
    1. add tool-versions
    2. pnpm create vite@latest ./temp -- --template vanilla
    3. move contents of temp up a level
    4. pnpm install
    5. pnpm install -D tailwindcss postcss autoprefixer
    6. pnpm tailwindcss init -p
    7. update tailwind config
    8. remove stuff from index.html and unused files

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function({ addBase, addUtilities }) {
+      addBase({
+        p: {
+          a: {
+            '@apply text-link': {},
+          },
+        }
+      });
+      addUtilities({
+        '.text-link': {
+          '@apply underline text-blue-500 transition-colors focus-visible:text-blue-400 hover:text-blue-400': {},
+        }
+      })
+    }),
+  ],
+}
+
